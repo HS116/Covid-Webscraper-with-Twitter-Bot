65e57a7036aeae4300cb092dd3d505c32f51307e
Neil Albuquerque <neilalbuquerque2002@gmail.com>
2022-05-18 13:35:47 +0200

    added functionality of automating creation of the actual json files with clear filename

--- json_creation.py
+++ json_creation.py
@@ -4,31 +4,26 @@ import json
 #TODO: store the change in indian covid cities covid data in json file. 
 #Issue is that the up arrow is represented as "/u2191", and we need to remove that, could u lamda function for that
 
-def create_json_files(country_covid_data, indian_cities_covidcases_data, indian_cities_covid_change_data):
+def create_json_file(data, file_name):
 
-    #convert the string values of each dictionary to integer to make it easier for future data analysis
-    #using dict() and list comprehension    
+    #Add a time stamp
+    from datetime import datetime
+    now = datetime.now()
+    timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+
+    data = convert_text_val_of_dict(data)
+    with open(f'jsonData/{file_name}_{timestamp}.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
     
-    country_covid_data = convert_text_val_of_dict(country_covid_data)
     
-
-    indian_cities_covidcases_data = convert_text_val_of_dict(indian_cities_covidcases_data)
-
-
-    indian_cities_covid_change_data = convert_text_val_of_dict(indian_cities_covid_change_data)
-
-    country_covid_data_json_object = json.dumps(country_covid_data, indent=4)
-    indian_cities_covidcases_json_object = json.dumps(indian_cities_covidcases_data, indent=4)
-    indian_cities_covid_change_json_object = json.dumps(indian_cities_covid_change_data, indent=4)
-
-    print(country_covid_data_json_object)
-    print(indian_cities_covidcases_json_object)
-    print(indian_cities_covid_change_json_object)
 
 def convert_text_to_int(text):
     #remove commas and the '\u2191' which represents the up arrow character
     return int(text.replace(',', '').replace('\u2191', ''))
 
 def convert_text_val_of_dict(dictionary):
+    #convert the string values of each dictionary to integer to make it easier for future data analysis
+    #using dict() and list comprehension    
     return dict([key, convert_text_to_int(value)] for key, value in dictionary.items())
 
--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from CountryCovidScraper import get_covid_data_from_worldometer as get_country_c
 from indianCitiesCovidScraper import get_indian_cities_covidcases_data
 from indianCitiesCovidScraper import get_indian_cities_covid_change_data
 from tweeting import tweet as tweet
-from json_creation import create_json_files
+from json_creation import create_json_file
 
 
 country_covid_data = get_country_covid_data()
 indian_cities_covidcases_data = get_indian_cities_covidcases_data()
 indian_cities_covid_change_data = get_indian_cities_covid_change_data()
 
-create_json_files(country_covid_data, indian_cities_covidcases_data, indian_cities_covid_change_data)
+
+create_json_file(country_covid_data, "Country specific total covid cases per 1M population ")
+create_json_file(indian_cities_covidcases_data, "Indian city specific total confirmed cases ")
+create_json_file(indian_cities_covid_change_data, "Indian city specific daily change in cases ")
 
 
 '''
